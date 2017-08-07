301b7d9ae7f06df98ae9a7fde01c2f43ba2bdda3
Corey <cry@Coreys-MacBook-Air.local>
2017-08-07 15:20:35 -0700

    Basic table skeleton

--- App.js
+++ App.js
@@ -1,10 +1,53 @@
-import react from React;
+import React from 'react';
+import ReactDOM from 'react-dom'
 
 class App extends React.Component {
-  
+  constructor(props) {
+    this.state = {
+      playerTurn: 1
+    };
+  }
+  render() {
+    return (
+      <table>
+        <tr>
+          <td>
+            a
+          </td>
+          <td>
+            b
+          </td>
+          <td>
+            c
+          </td>
+        <tr>
+          <td>
+          </td>
+          <td>
+          </td>
+          <td>
+          </td>
+        <tr>
+          <td>
+          </td>
+          <td>
+          </td>
+          <td>
+          </td>
+        </tr>
+        </tr>
+        </tr>
+        </table>
+    )
+  }
+
+
 }
 
-
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
 
 
 
